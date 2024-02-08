a1271f731905202d4ad249ac2c0960d33c7c373e
peppo95 <edop95@gmail.com>
2024-02-08 15:40:57 +0100

    Funzioni per queries
    la solita lista delle funzioni per connettersi al db con Python

---
+++ db/creazione_db.py
@@ -0,0 +1,27 @@
+from funzioni_sql import *
+
+connection = create_server_connection("localhost", "root", "")
+create_database(connection, "CREATE DATABASE museo")
+connection = create_db_connection("localhost", "root", "", "museo")
+
+artisti = """CREATE TABLE artisti(
+            id_artista int PRIMARY KEY AUTO_INCREMENT,
+            nome varchar(255) NOT NULL,
+            nazionalita varchar(255),
+            data_nascita int,
+            data_morte int);"""
+opere = """CREATE TABLE opere(
+            id_opera int PRIMARY KEY AUTO_INCREMENT,
+            titolo varchar(255) NOT NULL,
+            data_pubblicazione int,
+            url_immagine varchar(255));"""
+creazione = """CREATE TABLE creazione(
+                id int PRIMARY KEY AUTO_INCREMENT,
+                id_artista int NOT NULL,
+                id_opera int NOT NULL,
+                FOREIGN KEY (id_artista) REFERENCES artisti(id_artista),
+                FOREIGN KEY (id_opera) REFERENCES opere(id_opera));"""
+
+execute_query(connection, artisti)
+execute_query(connection, opere)
+execute_query(connection, creazione)
